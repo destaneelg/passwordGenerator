265192e0700d4ab2bd10c0567e9c5b12656792d2
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-06 16:08:30 -0500

    adding arrays and variables

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = confirm("Would you like upper case characters in your password?");
+var lowerCase = confirm("Would you like lower case characters in your password?");
+var numbers = confirm("Would you like numbers in your password?");
+var specialCharacter = confirm("Would you like special characters in your password?");
+var characterLength = prompt("How many characters would you like (between 8 characters and 128 characters)?");
+var numbersArr = ["1","2","3","4","5","6","7","8","9","0"]
+var letterArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var capitalLetterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var specialCharacterArr = ["!","@","#","$","%","^","&","*","(",")","-","=","+"]
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+function generatePassword(){
+  if (upperCase === true) {
+    return capitalLetterArr
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
