3c10fe3037a0d99e84d5eb09eea17674b63482db
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-06 20:40:44 -0500

    pushing homework, code does not quite work 2 different methods of trying in JS file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # passwordGenerator
+This is a password generator that allows you to choose criteria and then produces a randomized password. 
+The user can choose a length bewteen 8  and 128 characters, as well as choice of capitialization, numbers and/ or special characters.
+The prompts upon the launch of the webpage determine critera that goes into the password. 
--- script.js
+++ script.js
@@ -10,10 +10,33 @@ var letterArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var capitalLetterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var specialCharacterArr = ["!","@","#","$","%","^","&","*","(",")","-","=","+"]
 
-
 function generatePassword(){
-  if (upperCase === true) {
-    return capitalLetterArr
+for (i = 0; i < characterLength; i++) {
+  if (upperCase === true){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+  }
+  else if (lowerCase === true){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+  }
+  else if (numbers === true){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+  }
+  if (specialCharacter === true){
+    return String.fromCharCode(Math.floor(Math.random() * 33) + 14)
+  }
+}
+for (i = 0; i < characterLength; i++) {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+}
+function validation() {
+  if (characterLength.length < 8 || characterLength.length > 128) {
+    return "Please enter valid length!"
   }
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
