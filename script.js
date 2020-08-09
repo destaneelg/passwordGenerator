d4accbe9e26aba9d2d82a63b16c74aa9900a58a3
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-09 08:47:06 -0500

    changing to arrays apptempted for loop

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbersArr = "1234567890";
+var letterArr = "abcdefghijklmnopqrstuvwxyz";
+var capitalLetterArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacterArr = "!@#$%^&*()-=+";
+
+var characterLength = prompt("How many characters would you like...between 8 characters and 128 characters?");
 var upperCase = confirm("Would you like upper case characters in your password?");
 var lowerCase = confirm("Would you like lower case characters in your password?");
 var numbers = confirm("Would you like numbers in your password?");
-var specialCharacter = confirm("Would you like special characters in your password?");
-var characterLength = prompt("How many characters would you like (between 8 characters and 128 characters)?");
-var numbersArr = ["1","2","3","4","5","6","7","8","9","0"]
-var letterArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var capitalLetterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var specialCharacterArr = ["!","@","#","$","%","^","&","*","(",")","-","=","+"]
+var specialCharacter = confirm("Would you like special characters in your password");
 
-function generatePassword(){
+// Write password to the #password input
+function writePassword() {}
+  var passwordCriteria = "";
+  var password = ""
+ function generatePassword() {
+  if (capitalLetterArr ===true){
+    passwordCriteria = passwordCriteria + capitalLetterArr
+  }
+    if (letterArr ===true){
+      passwordCriteria = passwordCriteria + letterArr
+    }
+    if (numbersArr===true){
+      passwordCriteria = passwordCriteria + numbersArr;
+    }
+    if (specialCharacterArr ===true){
+      passwordCriteria = passwordCriteria + specialCharacterArr;
+    }
+generatePassword();
 for (i = 0; i < characterLength; i++) {
-  if (upperCase === true){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-  }
-  else if (lowerCase === true){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-  }
-  else if (numbers === true){
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-  }
-  if (specialCharacter === true){
-    return String.fromCharCode(Math.floor(Math.random() * 33) + 14)
-  }
+  password += passwordCriteria[Math.floor(Math.random() * characterLength)];
 }
-for (i = 0; i < characterLength; i++) {
-  var password = generatePassword();
+return password
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
-}
-}
-function validation() {
-  if (characterLength.length < 8 || characterLength.length > 128) {
-    return "Please enter valid length!"
+
   }
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
