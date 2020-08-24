0f2c7350e19af81c4a9280e60e0d6bae0081ae53
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-24 18:54:13 -0500

    adding changes to homework

--- script.js
+++ script.js
@@ -12,33 +12,53 @@ var numbers = confirm("Would you like numbers in your password?");
 var specialCharacter = confirm("Would you like special characters in your password");
 
 // Write password to the #password input
-function writePassword() {}
-  var passwordCriteria = "";
-  var password = ""
- function generatePassword() {
-  if (capitalLetterArr ===true){
-    passwordCriteria = passwordCriteria + capitalLetterArr
+function generatePasswordOptions() {
+  console.log("test");
+  //defining the character length
+  if(isNaN(characterLength)=== true){
+    alert("You must enter a number");
+    return;
+  };
+
+  if(characterLength < 8) {
+    alert("password  length must be at least eight characters");
+     return;
+  };
+  if(characterLength > 128 ) {
+    alert("password length cannot be over 128 characters");
+     return;
+  };
+  var passwordCriteria = {
+    characterLength: characterLength,
+    upperCase:upperCase,
+    lowerCase:lowerCase,
+    numbers:numbers,
+    specialCharacter:specialCharacter,
   }
-    if (letterArr ===true){
-      passwordCriteria = passwordCriteria + letterArr
-    }
-    if (numbersArr===true){
-      passwordCriteria = passwordCriteria + numbersArr;
-    }
-    if (specialCharacterArr ===true){
-      passwordCriteria = passwordCriteria + specialCharacterArr;
-    }
-generatePassword();
-for (i = 0; i < characterLength; i++) {
-  password += passwordCriteria[Math.floor(Math.random() * characterLength)];
+  console.log(passwordCriteria);
+  return passwordCriteria;
 }
-return password
+function createPassword() {
+ var options = generatePasswordOptions();
+ var passwordArr = [];
+ var possibleCharacters = [];
+ var charactersChoosen = [];
+ if(options.upperCase){
+  console.log(options.upperCase);
+  possibleCharacters = possibleCharacters.concat(capitalLetterArr)
+  console.log(possibleCharacters, "characters")
+  //push into the charactersChoosen array with the random characters
+  
+ };
+}
+//Random function generator using math.floor and math.random to return random characters for array, 
+function writePassword() {
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
   
-
   passwordText.value = password;
 
-  }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
